3860fef3eb4f944f163b7832556534ac42bea964
ttran9 <toddtran9@gmail.com>
2017-11-20 19:45:20 -0800

    Initial commit.

---
+++ lib/e_gadd_view_tool.rb
@@ -0,0 +1,6 @@
+require "e_gadd_view_tool/version"
+require "e_gadd_view_tool/renderer"
+
+module EGaddViewTool
+  # Your code goes here...
+end
---
+++ lib/e_gadd_view_tool/version.rb
@@ -0,0 +1,3 @@
+module EGaddViewTool
+  VERSION = "0.1.0"
+end
